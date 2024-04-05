e743a89f5baf1cf4bb14807915973800caa91aca
KAsare1 <asarek977@gmail.com>
2024-04-05 22:01:44 +0000

    minor changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -30,14 +30,16 @@ export default function Header() {
                                     </div>
                                 </div>                                 
                             </li>
-                            {/* <li>
-                            <select className="border border-opacity-20 rounded text-center border-neutral-700 block py-2 px-3 ml-20 font-montserrat">
+                            <li>
+                            {/* <select className="border border-opacity-20 rounded text-center border-neutral-700 block py-2 px-3 ml-20 font-montserrat">
                                 <option value='select your category' className="text-center py-2 px-3 font-montserrat">
+                                <svg className="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" stroke-width="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use xlink:href="#a" y="420"/><use xlink:href="#a" y="840"/><use xlink:href="#a" y="1260"/></g><use xlink:href="#a" y="1680"/></g><use xlink:href="#b" x="247" y="210"/></g><use xlink:href="#c" x="494"/></g><use xlink:href="#d" x="988"/><use xlink:href="#c" x="1976"/><use xlink:href="#e" x="2470"/></g></svg>
+       
                                     English
                                 </option>
                                 <option value="volvo" className="text-center py-2 px-3 font-montserrat">French</option>
-                            </select>
-                            </li> */}
+                            </select> */}
+                            </li>
                         </ul>
                         
                     </div>
