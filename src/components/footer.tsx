19ca23707c346ccf6c8571b5601db5c8cdafea68
KAsare1 <asarek977@gmail.com>
2024-04-07 20:34:51 +0000

    final

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,8 +9,7 @@ export default function Footer(){
 
 <footer className="bg-[#8E2C2C]">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-100 sm:text-center dark:text-gray-100">© 2024 <a href="#" className="hover:underline">Cloudsoft Technologies</a>. All Rights Reserved.
-    </span>
+
     <ul className="flex flex-wrap items-center mt-3 text-m font-medium text-gray-100 sm:mt-0">
     <li>
             <a href="#" className="hover:underline me-4 md:me-6">Company</a>
@@ -28,6 +27,8 @@ export default function Footer(){
             <a href="#" className="hover:underline">Contact</a>
         </li>
     </ul>
+    <span className="text-sm text-gray-100 sm:text-center dark:text-gray-100">© 2024 <a href="#" className="hover:underline">Cloudsoft Technologies</a>. All Rights Reserved.
+    </span>
     </div>
 
     <div className="px-4 py-6 bg-gray md:flex md:items-center md:justify-center">
